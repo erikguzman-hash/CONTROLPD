c126b679522a56eec1f5ee6fd47e56b6af38e346
Erik Guzman <erikguzman@hgio.co>
2025-09-04 10:25:20 -0500

    Secure firebase configuration

---
+++ .vscode/firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB8-KNE_60SD9OmavwOG816L5dGxOAgzO4",
+  authDomain: "weekpd.firebaseapp.com",
+  projectId: "weekpd",
+  storageBucket: "weekpd.firebasestorage.app",
+  messagingSenderId: "712077553599",
+  appId: "1:712077553599:web:07fa4b6309af0e50ecfb0a",
+  measurementId: "G-CXM1ZQ7VFL"
+};
